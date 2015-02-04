a3e471fbfd72a46597716ecc76fa6f4826175751
s41m1r <saimir.bala@gmail.com>
2015-02-04 18:26:13 +0100

    Drawn an empty Gantt chart

---
+++ MiningSVN/src/gui/MyFirstGanttChart.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package gui;
+
+/**
+ * @author Saimir Bala
+ *
+ */
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+ 
+import com.flexganttfx.view.GanttChart;
+ 
+public class MyFirstGanttChart extends Application {
+ 
+  @Override
+  public void start(Stage stage) throws Exception {
+ 
+    // <- Our Gantt chart
+    GanttChart<?> gantt = new GanttChart<>();
+     
+    Scene scene = new Scene(gantt);
+     
+    stage.setScene(scene);
+    stage.centerOnScreen();
+    stage.sizeToScene();
+    stage.show();   
+  }
+ 
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
+}
