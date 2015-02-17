dd6bee1a934e1cae09072687d35319c6e33c673b
s41m1r <saimir.bala@gmail.com>
2015-02-17 21:49:40 +0100

    Commit and tested again closeness matrix

--- MiningSVN/src/distance/ClosenessMatrix.java
+++ MiningSVN/src/distance/ClosenessMatrix.java
@@ -5,7 +5,6 @@ package distance;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.DecimalFormat;
