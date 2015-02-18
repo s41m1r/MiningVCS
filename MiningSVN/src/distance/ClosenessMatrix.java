ac60ae2a458cb2c83add24f8de2b243d9c9de601
s41m1r <saimir.bala@gmail.com>
2015-02-18 18:50:20 +0100

    Some visualization

--- MiningSVN/src/distance/ClosenessMatrix.java
+++ MiningSVN/src/distance/ClosenessMatrix.java
@@ -5,10 +5,7 @@ package distance;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
