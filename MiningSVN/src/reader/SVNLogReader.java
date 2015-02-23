418091d904a142dfd0e933d1102c93876a49c0b3
s41m1r <saimir.bala@gmail.com>
2015-02-23 17:03:32 +0100

    class util.Tree to be modified

--- MiningSVN/src/reader/SVNLogReader.java
+++ MiningSVN/src/reader/SVNLogReader.java
@@ -26,50 +26,50 @@ import model.svn.SVNLogEntry;
  *
  */
 public class SVNLogReader implements LogReader<LogEntry>, Closeable {
-	
+
 	private BufferedReader br = null;		
 	/**
-    * 
-    */
-   public SVNLogReader(BufferedReader reader) {
-	   br = reader;
-   }
-   
-   public SVNLogReader(File file) throws FileNotFoundException {
-	   br = new BufferedReader(new FileReader(file));
-   }
-   /**
-    * @throws FileNotFoundException 
-    * 
-    */
-   public SVNLogReader(String fileName) throws FileNotFoundException {
-   	br = new BufferedReader(new FileReader(fileName));
-   }
-	
-   @Override
-   public List<LogEntry> readAll() throws IOException {
+	 * 
+	 */
+	public SVNLogReader(BufferedReader reader) {
+		br = reader;
+	}
 
-	   List<LogEntry> logEntries = new ArrayList<LogEntry>();
-	   LogEntry entry;
+	public SVNLogReader(File file) throws FileNotFoundException {
+		br = new BufferedReader(new FileReader(file));
+	}
+	/**
+	 * @throws FileNotFoundException 
+	 * 
+	 */
+	public SVNLogReader(String fileName) throws FileNotFoundException {
+		br = new BufferedReader(new FileReader(fileName));
+	}
 
-	   while((entry=readNext())!=null)
-	   	logEntries.add(entry);
-	   
-	   return logEntries;
-   }
+	@Override
+	public List<LogEntry> readAll() throws IOException {
+
+		List<LogEntry> logEntries = new ArrayList<LogEntry>();
+		LogEntry entry;
+
+		while((entry=readNext())!=null)
+			logEntries.add(entry);
+
+		return logEntries;
+	}
 
 	/* (non-Javadoc)
 	 * @see reader.LogReader#readNext()
 	 */
-   @Override
-   public LogEntry readNext() throws IOException {
-   	
-   	String line = br.readLine();
-   	
-   	if(line==null)
-   		return null;
-		
-   	String revision = line.split("Revision: ")[1];
+	@Override
+	public LogEntry readNext() throws IOException {
+
+		String line = br.readLine();
+
+		if(line==null)
+			return null;
+
+		String revision = line.split("Revision: ")[1];
 		line = br.readLine();
 		String author = line.split("Author: ")[1];
 		line = br.readLine();
@@ -80,46 +80,46 @@ public class SVNLogReader implements LogReader<LogEntry>, Closeable {
 			throw new IOException();
 		}
 		String message = readMessage(br); 
-		
+
 		List<Change> changeList = new ArrayList<Change>();
-		
+
 		while(!(line = br.readLine()).trim().equals("")){
 			String[] changeLine = line.split(" : ");
 			Change ch = new Change(changeLine[0].trim(), changeLine[1].trim());
 			changeList.add(ch);
 		}
-		
+
 		Locale locale = new Locale("de", "AT", "Austria");
 		DateTimeFormatter germanFmt = DateTimeFormat.forPattern("EEEE, dd. MMMM yyyy HH:mm:ss").withLocale(locale);
-	    DateTime date = germanFmt.parseDateTime(dateString);
-	   
+		DateTime date = germanFmt.parseDateTime(dateString);
+
 		SVNLogEntry svnLogEntry = new SVNLogEntry(revision,author,date,message,changeList);
-		
+
 		return svnLogEntry;
-   }
+	}
 
 	/**
 	 * @param br2
 	 * @return
 	 * @throws IOException 
 	 */
-   private static String readMessage(BufferedReader br2) throws IOException {
-   	String line = br2.readLine().trim();
-   	String msg = "";
-   	
-   	while(line!=null && !line.equals("----")){
-   		msg+=line;
-   		line = br2.readLine().trim();
-   	}
-	   return msg;
-   }
+	private static String readMessage(BufferedReader br2) throws IOException {
+		String line = br2.readLine().trim();
+		String msg = "";
+
+		while(line!=null && !line.equals("----")){
+			msg+=line;
+			line = br2.readLine().trim();
+		}
+		return msg;
+	}
 
 	/* (non-Javadoc)
 	 * @see java.io.Closeable#close()
 	 */
-   @Override
-   public void close() throws IOException {
-   	br.close();
-   }
+	@Override
+	public void close() throws IOException {
+		br.close();
+	}
 
 }
--- MiningSVN/src/util/Tree.java
+++ MiningSVN/src/util/Tree.java
@@ -3,8 +3,22 @@
  */
 package util;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.StringTokenizer;
+
+import model.Change;
+import model.Event;
+import model.Log;
+import model.LogEntry;
+import model.svn.SVNLog;
+import reader.LogReader;
+import reader.SVNLogReader;
 
 /**
  * @author Saimir Bala
@@ -12,87 +26,108 @@ import java.util.StringTokenizer;
  */
 class Tree
 {
-    class Node
-    {
-        String data;
-        ArrayList<Node> children;
+	class Node
+	{
+		String data;
+		ArrayList<Node> children;
 
-        public Node(String data)
-        {
-            this.data = data;
-            children = new ArrayList<Node>();
-        }
+		public Node(String data)
+		{
+			this.data = data;
+			children = new ArrayList<Node>();
+		}
 
-        public Node getChild(String data)
-        {
-            for(Node n : children)
-                if(n.data.equals(data))
-                    return n;
+		public Node getChild(String data)
+		{
+			for(Node n : children)
+				if(n.data.equals(data))
+					return n;
 
-            return null;
-        }
+			return null;
+		}
 
 		@Override
-      public String toString() {
-	      return "Node [data=" + data + ", children=" + children + "]";
+		public String toString() {
+			return "Node [data=" + data + ", children=" + children + "]";
+		}
+	}
+
+	private Node root;
+
+	public Tree()
+	{
+		root = new Node("");
+	}
+
+	public boolean isEmpty()
+	{
+		return root==null;
+	}
+
+	public void add(String str)
+	{
+		Node current = root;
+		Scanner s = new Scanner(str);
+		s.useDelimiter("/");//no whitespace preceding "/"
+		s.skip("[Copy from path].*");
+		while(s.hasNext())
+		{
+			str = s.next();
+			
+			Node child = current.getChild(str);
+			if(child==null)
+			{
+				current.children.add(new Node(str));
+				child = current.getChild(str);
+			}
+			current = child;
+		}
+		s.close();
+	}
+
+	public void print()
+	{
+		print(this.root, 0);
+	}
+
+	private void print(Node n, int level)
+	{
+		if(n==null)
+			return;
+		for(Node c : n.children)
+		{
+			for(int l=level;l>0;l--)
+				System.out.print(" ");
+			System.out.print("+-");
+			System.out.println("["+level+"] "+c.data + " ");
+			print(c, level+1);
+		}
+	}
+
+	public static void main(String[] args) throws IOException
+	{
+		Tree t = new Tree();
+		LogReader<LogEntry> lr = new SVNLogReader("resources/20150129_SNV_LOG_FROM_SHAPE_PROPOSAL_new.log");
+		Log log = new SVNLog(lr.readAll());
+//		TestLog.toFile("/home/saimir/out.txt");
+		System.out.println("Read "+log.size()+" entries.");
+		Map<String, List<Event>> fem = FileEventMap.buildHistoricalFileEventMap(log);
+		lr.close();
+		Collection<Change> c = log.getAllChanges();
+		for (Change ch : c) {
+	      System.out.println(ch.getPath());
       }
-    }
-
-    private Node root;
-
-    public Tree()
-    {
-        root = new Node("");
-    }
-
-    public boolean isEmpty()
-    {
-        return root==null;
-    }
-
-    public void add(String str)
-    {
-        Node current = root;
-        StringTokenizer s = new StringTokenizer(str, "/");
-        while(s.hasMoreElements())
-        {
-            str = (String)s.nextElement();
-            Node child = current.getChild(str);
-            if(child==null)
-            {
-                current.children.add(new Node(str));
-                child = current.getChild(str);
-            }
-            current = child;
-        }
-    }
-
-    public void print()
-    {
-        print(this.root, 0);
-    }
-
-    private void print(Node n, int level)
-    {
-        if(n==null)
-            return;
-        for(Node c : n.children)
-        {
-            System.out.print(level+" "+c.data + " ");
-            print(c, level+1);
-        }
-    }
-
-    public static void main(String[] args)
-    {
-        Tree t = new Tree();
-        t.add("/templates");
-        t.add("/slides");
-        t.add("The World/Asia/Iran");
-        t.add("/templates/checkliste_kostenplan.doc");
-        t.add("The World/Asia/China");    // Even if you insert only this statement.
-                                          // You get the desired output, 
-                                          // As any string not found is inserted
-        t.print();
-    }
+		Set<String> files = fem.keySet();
+		for (String string : files) {
+			t.add(string);
+		}
+//		t.add("/templates");
+//		t.add("/slides");
+//		t.add("The World/Asia/Iran");
+//		t.add("/templates/checkliste_kostenplan.doc");
+//		t.add("The World/Asia/China");    // Even if you insert only this statement.
+//		// You get the desired output, 
+//		// As any string not found is inserted
+		t.print();
+	}
 }
