941fa7774066554e7b580851b36988587364676f
saimir <saimir@saimir-ThinkPad-T410>
2015-11-23 14:05:10 +0100

    Added test file

--- MiningSVN/src/test/git/TestGitAPI.java
+++ MiningSVN/src/test/git/TestGitAPI.java
@@ -3,10 +3,16 @@ package test.git;
 import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.LogCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoHeadException;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 public class TestGitAPI {
@@ -14,7 +20,7 @@ public class TestGitAPI {
 	public static void main(String[] args) {
 		try {
 			Repository repo = new FileRepositoryBuilder()
-			.setGitDir(new File("/home/saimir/git/hoosegow/.git"))
+			.setGitDir(new File("/home/saimir/git/MiningCVS/.git"))
 			.build();
 			
 			// Get a reference
@@ -24,13 +30,31 @@ public class TestGitAPI {
 			ObjectId masterTip = master.getObjectId();
 
 			// Rev-parse
-			ObjectId obj = repo.resolve("HEAD^{tree}");
+			ObjectId obj = repo.resolve("id^1^{tree}");
 
 			// Load raw object contents
 			ObjectLoader loader = repo.open(masterTip);
 			loader.copyTo(System.out);
 			
+			Git git = new Git(repo);
+			Iterable<RevCommit> log = git.log().call();
+			
+			for (RevCommit revCommit : log) {
+				System.out.println(revCommit.getName()+" "+revCommit.getCommitTime()+" "+revCommit.getShortMessage());
+			}
+			
+			LogCommand logCommand = git.log();
+			RevWalk revWalk = new RevWalk(repo);
+			
+			
+			
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoHeadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GitAPIException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
